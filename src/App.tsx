2035b93226d7a59832d83b68818755b03512d471
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-12-22 15:30:16 +0800

    started setting up Shop page

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,14 @@ import Welcome from './components/Welcome';
 import PrivacyPolicy from './components/PrivacyPolicy';
 import Disclaimer from './components/Disclaimer';
 import Home from './components/pages/Home';
+import Shop from './components/pages/Shop';
 
 const router = createBrowserRouter([
   { path: '/', element: <Welcome />, errorElement: <ErrorPage />},
   { path: '/privacy-policy', element: <PrivacyPolicy /> },
   { path: '/disclaimer', element: <Disclaimer /> },
-  { path: '/home', element: <Home /> }
+  { path: '/home', element: <Home /> },
+  { path: '/shop', element: <Shop /> },
 ])
 
 function App() {
---
+++ src/components/pages/Shop.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Template from '../layout/Template';
+import Breadcrumb from '../shared/Breadcrumb';
+
+function Shop() {
+
+    return (
+        <>
+            <Template>
+                <div className="row">
+                    <div className="col-12">
+                        <Breadcrumb />
+                    </div>
+                    <div className="col-12">
+                        
+                    </div>
+                </div>
+            </Template>
+        </>
+    )
+}
+
+export default Shop;
