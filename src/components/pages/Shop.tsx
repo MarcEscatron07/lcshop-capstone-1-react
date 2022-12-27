2838fd503f8ed32da0b26b9326f36bffc0bad3fd
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-12-27 22:33:14 +0800

    added SortBy component

--- src/components/pages/Shop.tsx
+++ src/components/pages/Shop.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Template from '../layout/Template';
 import Breadcrumb from '../shared/Breadcrumb';
 import Tabs from '../shared/Tabs';
+import SortBy from '../shared/SortBy';
 import Searchbar from '../shared/Searchbar';
 
 const tabs: any[] = [
@@ -18,17 +19,82 @@ const tabs: any[] = [
         tabName: 'Accessories',
         tabHeader: 'LCS Accessories'
     }
-]
+];
+
+const sortBy: any[] = [
+    {
+        name: 'LATEST',
+        value: 'sort-latest'
+    },
+    {
+        name: 'FEATURED',
+        value: 'sort-featured'
+    },
+    {
+        name: 'BEST SELLING',
+        value: 'sort-best-selling'
+    },
+    {
+        name: 'ALPHABETICAL, A-Z',
+        value: 'sort-alpha-asc'
+    },
+    {
+        name: 'ALPHABETICAL, Z-A',
+        value: 'sort-alpha-desc'
+    },
+    {
+        name: 'PRICE (ASCENDING)',
+        value: 'sort-price-asc'
+    },
+    {
+        name: 'PRICE (DESCENDING)',
+        value: 'sort-price-desc'
+    },
+];
+
+
 
 function Shop() {
     const [shopTab, setShopTab] = useState<string>(tabs[0]?.tabName);
+    const [shopSort, setShopSort] = useState<string>(sortBy[0]?.value);
+    const [shopSearch, setShopSearch] = useState<string>('');
 
     useEffect(() => {
         console.log('Shop > shopTab val', shopTab)
     }, [shopTab])
 
+    useEffect(() => {
+        console.log('Shop > shopSort val', shopSort)
+    }, [shopSort])
+
+    useEffect(() => {
+        console.log('Shop > shopSearch val', shopSearch)
+    }, [shopSearch])
+
     const handleSelectTab = (currentTab: string) => {
         setShopTab(currentTab);
+    }
+
+    const handleSelectSort = (value: string) => {
+        // console.log('handleSelectSort > value', value)
+        setShopSort(value);
+    }
+
+    const handleClickSearch = (value: string) => {
+        setShopSearch(value);
+    }
+
+    function renderTabContent(tab: any, tabIdx: number) {
+        if(shopTab === tab?.tabName) {
+            return (
+                <div key={tabIdx} className="d-flex justify-content-between align-items-center">
+                    <h3 className="mb-2">{tab?.tabHeader}</h3>
+                    <SortBy sortByList={sortBy} defaultSort={sortBy && sortBy[0]?.value} onSelectSort={handleSelectSort} />
+                </div>
+            )
+        } else {
+            return null;
+        }
     }
 
     return (
@@ -39,34 +105,28 @@ function Shop() {
                         <Breadcrumb />
                     </div>
                     <div className="col-12 px-5 py-4">
-                        <div className="row mb-4">
+                        <div className="row mb-5">
                             <div className="col d-md-flex justify-content-between">
                                 <div className="d-md-flex py-2">
                                     <Tabs tabsList={tabs} defaultTab={tabs && tabs[0]?.tabName} onSelectTab={handleSelectTab} />
                                 </div>
                                 <div className="d-md-flex py-2">
-                                    <Searchbar />
+                                    <Searchbar onClickSearch={handleClickSearch}/>
                                 </div>                                
+                            </div>
+                        </div>
+                        <div className="row line-separator pb-4">
+                            <div className="col">
+                                <div className="tab-panel">
+                                    {tabs.map((tab, tabIdx) => {
+                                        return renderTabContent(tab, tabIdx);
+                                    })}
+                                </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col">
-                                {/* {shopTab === tabs[0]?.tabName ? (
-                                    <>
-                                        <h3 className="mb-2">{tabs[0]?.tabHeader}</h3>
-                                    </>
-                                ) : null} */}
-                                {/* {tabs.map((tab) => {
-                                    if(shopTab === tabs[0]?.tabName) {
-                                        return (
-                                            <>
-                                                <h3 className="mb-2">{tabs[0]?.tabHeader}</h3>
-                                            </>
-                                        )
-                                    } else {
-                                        return null;
-                                    }
-                                })} */}
+                                {/* Shop items here (Jerseys, Casual Wear, Accessories) */}
                             </div>
                         </div>
                     </div>
---
+++ src/components/shared/SortBy.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+interface IProps {
+    sortByList: any[],
+    defaultSort: string,
+    onSelectSort: any
+}
+
+function SortBy(props: IProps) {
+    const [currentSort, setCurrentSort] = useState<string>(props?.defaultSort ?? '');
+
+    useEffect(() => {
+        // console.log('currentSort > val', currentSort)
+    }, [currentSort])
+
+    const onSortAction = (e: any) => {
+        setCurrentSort(e?.target?.value);
+        if(props.onSelectSort) {
+            props.onSelectSort(e?.target?.value);
+        }
+    }
+    
+    return (
+        <div className="sortby-wrapper">
+            <label className="my-0 mx-2 fw-bold" htmlFor="sort-by">Sort By</label>
+            <select className="sortby-select" name="sort" id="sort-by" onChange={onSortAction}>
+                {props.sortByList.map((sort, sortIdx) => {
+                    return (
+                        <option key={sortIdx} value={sort?.value}>{sort?.name}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+SortBy.propTypes = {
+    sortByList: PropTypes.array.isRequired,
+    defaultSort: PropTypes.string.isRequired,
+    onSelectSort: PropTypes.func.isRequired
+}
+
+export default SortBy;
