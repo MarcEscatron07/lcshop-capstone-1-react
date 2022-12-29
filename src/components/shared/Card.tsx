ab0888eb68cace4bb79e4255f6c7e90062459e44
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-12-29 20:19:06 +0800

    finished Shop page excluding FE functionalities

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -15,21 +15,21 @@ interface IProps {
 function Card(props: IProps) {
     const [imgSrc, setImgSrc] = useState<string>(props.src);
 
-    const cardImgClassname: string = props.imgObj ? 'card-image' : '';
-    const cardBodyClassname: string = !props.cardLayout || props.cardLayout === 'default' ? 'card-default' : 'card-centered';
+    const cardImgClassname: string = !props.imgObj?.back ? ' faded' : '';
+    const cardBodyClassname: string = !props.cardLayout || props.cardLayout === 'default' ? ' card-default' : ' card-centered';
 
     useEffect(() => {
         // console.log('Card > imgSrc', imgSrc)
     }, [imgSrc])    
 
     const handleOnMouseEnter = () => {
-        if((props.imgObj && props.imgObj?.back) && cardImgClassname === 'card-image') {
+        if(props.imgObj && props.imgObj?.back) {
             setImgSrc(props.imgObj?.back);
         }
     }
 
     const handleOnMouseLeave = () => {
-        if((props.imgObj && props.imgObj?.front) && cardImgClassname === 'card-image') {
+        if(props.imgObj && props.imgObj?.front) {
             setImgSrc(props.imgObj?.front);
         }
     }
@@ -37,15 +37,17 @@ function Card(props: IProps) {
     return (
         <>
             <div className="card h-100">
-                <img src={imgSrc} className={`card-img-top img-fluid ${cardImgClassname}`} alt={props.alt} onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave} />
-            <div className="card-body">
-                <h5 className={`card-title ${cardBodyClassname}`}>{props.title}</h5>
-                {props.description && props.description.trim() !== '' ? (<p className={`card-text ${cardBodyClassname}`}>{props.description}</p>) : null}
-            </div>
-            <div className="card-footer">
-                <small className="text-muted fw-bold">{props?.price ?? '$0.00'}</small>
-                <button className="card-button btn btn-primary">{props?.buttonText ?? 'OK'}</button>                                                    
-            </div>
+                <div className="p-3">
+                    <img src={imgSrc} className={`card-image card-img-top img-fluid${cardImgClassname}`} alt={props.alt} onMouseEnter={handleOnMouseEnter} onMouseLeave={handleOnMouseLeave} />
+                </div>
+                <div className="card-body">
+                    <h5 className={`card-title${cardBodyClassname}`}>{props.title}</h5>
+                    {props.description && props.description.trim() !== '' ? (<p className={`card-text${cardBodyClassname}`}>{props.description}</p>) : null}
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted fw-bold">{props?.price ?? '$0.00'}</small>
+                    <button className="card-button btn btn-primary">{props?.buttonText ?? 'OK'}</button>                                                    
+                </div>
             </div>
         </>
     )
