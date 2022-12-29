aca39e967a040b0c9768d35f507deb322e73b438
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-12-29 21:02:00 +0800

    removed unused import

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Template from '../layout/Template';
 import Card from '../shared/Card';
